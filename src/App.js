ef928f7f211073cffab82f7d7b49547866f62e63
Chedly <chedly.marzougui@gmail.com>
2021-06-23 15:24:29 +0100

    create  react state checkpoint project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      person: {
+        fullName: "Chedly",
+        bio: "My Biography",
+        profession: "Full stack js developer",
+      },
+      show: true,
+      interval: 0,
+    };
+  }
+
+  togglePerson = () => {
+    this.setState((prevState) => ({
+      show: !prevState.show,
+    }));
+    this.setState(() => ({
+      interval: 0,
+    }));
+  };
+
+  componentDidMount = () => {
+    setInterval(() => {
+      this.setState((prevState) => ({
+        interval: prevState.interval + 1,
+      }));
+    }, 1000);
+  };
+
+  // componentDidMount = () => {
+  //   this.setState(() => ({
+  //     interval: 0,
+  //   }));
+  // };
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.show ? this.state.person.fullName : ""}</h1>
+        <h2>{this.state.show ? this.state.person.profession : ""}</h2>
+        <p>{this.state.show ? this.state.person.bio : ""}</p>
+        <button onClick={() => this.togglePerson()}>toggle</button>
+        <h2>{this.state.interval}</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
